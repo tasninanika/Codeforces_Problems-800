f9d4819e0ef8e474f564179e8cc4e2ba97fa3633
anikatasnin <jarintasnin09@gmail.com>
2025-01-06 11:38:15 +0600

    added  Anton and Danik

--- Anton and Danik/antonanddanik.cpp
+++ Anton and Danik/antonanddanik.cpp
@@ -1,1 +1,31 @@
+#include<iostream>
+using namespace std;
 
+int main(){
+    long long n, A = 0, D = 0;
+    cin >> n;
+
+    string s;
+    cin >> s;
+
+    for(int i = 0; i < s.size(); i++){
+        if(s[i] == 'A'){
+            A++;
+        }
+        else{
+            D++;
+        }
+    }
+
+    if(A > D){
+            cout << "Anton" << endl;
+        }
+        else if(A < D){
+            cout << "Danik" << endl;
+        }
+        else{
+            cout << "Friendship" << endl;
+        }
+
+    return 0;
+}
