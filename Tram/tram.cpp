81995c354e08c4febf0bf7f6ebb4fdc64ba67213
anikatasnin <jarintasnin09@gmail.com>
2025-01-07 21:03:25 +0600

    Added Tram

--- Tram/tram.cpp
+++ Tram/tram.cpp
@@ -1,1 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n, a, b, passangers = 0, mini = 0;
+    cin >> n;
+
+    for(int i = 0; i < n; i++){
+
+        cin >> a >> b;
+        passangers = passangers - a + b;
+
+        if(passangers > mini){
+            mini = passangers;
+        }
+    }
+
+    cout << mini << endl;
+
+
+
+    return 0;
+}
 
