eaedcf7317e521becc41c451eb6d4d36762a2b4b
anikatasnin <jarintasnin09@gmail.com>
2025-01-09 11:00:52 +0600

    Added Queue at the School

--- Queue at the School/queueattheschool.cpp
+++ Queue at the School/queueattheschool.cpp
@@ -9,10 +9,12 @@ int main(){
     string s;
     cin >> s;
 
-    for(int i = 0; i < s.size(); i++){
-        if(s[i] == 'B' && s[i+1] == 'G'){
-            swap(s[i],s[i+1]);
-        }
+    for(int i = 0; i < t; i++){
+        for(int i = 0; i < s.size(); i++){
+            if(s[i] == 'B' && s[i+1] == 'G'){
+                swap(s[i],s[i+1]);
+            }
+    }
     }
 
     cout << s << endl;
