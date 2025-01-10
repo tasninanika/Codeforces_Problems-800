9435e7b5129407e0dea8ec6eba61dc44e5b9e598
anikatasnin <jarintasnin09@gmail.com>
2025-01-10 20:37:53 +0600

    Added In Search of an Easy Problem

--- In Search of an Easy Problem/insearchofaneasyproblem.cpp
+++ In Search of an Easy Problem/insearchofaneasyproblem.cpp
@@ -2,12 +2,14 @@
 using namespace std;
 
 int main(){
-    int n, arr[n];
+    int n, arr[n], sum = 0;
     cin >> n;
 
     for(int i = 0; i < n; i++){
         cin >> arr[i];
+        sum += arr[i];
     }
+
 
 
     return 0;
