ed69443a44f09d86714fabd5a71e8020ec67932d
anikatasnin <jarintasnin09@gmail.com>
2025-01-12 22:36:28 +0600

    Added George and Accommodation

--- George and Accommodation/georgeandaccommodation.cpp
+++ George and Accommodation/georgeandaccommodation.cpp
@@ -2,12 +2,14 @@
 using namespace std;
 
 int main(){
-    int n, person, capacity;
+    int n, person, capacity, c = 0;
     cin >> n;
 
     for(int i = 0; i < n; i++){
         cin >> capacity >> person;
-        if(person - capacity >= 2)
+        if(person - capacity >= 2){
+            c++
+        }
     }
 
     return 0;
