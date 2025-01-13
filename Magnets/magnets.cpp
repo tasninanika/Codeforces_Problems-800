6fde8c0822df0a3bb946925e189ebf39f72bd001
anikatasnin <jarintasnin09@gmail.com>
2025-01-13 14:15:32 +0600

    Added Magnets

--- Magnets/magnets.cpp
+++ Magnets/magnets.cpp
@@ -6,11 +6,16 @@ int main(){
     cin >> n;
 
     int arr[n];
+    int c = 0;
 
     for(int i = 0; i < n; i++){
         cin >> arr[i];
-
+        if(arr[i] == arr[i+1]){
+            c++;
+        }
     }
+
+    cout << c << endl;
 
 
 
