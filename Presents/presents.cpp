cd989a00571f3c7fafbf3a837981e1bd35ecbeb4
anikatasnin <jarintasnin09@gmail.com>
2025-01-14 22:29:39 +0600

    Added Presents

--- Presents/presents.cpp
+++ Presents/presents.cpp
@@ -8,14 +8,14 @@ int main(){
 
     int arr[n];
 
-    for(int i = 1; i <= n; i++){
+    for(int i = 0; i < n; i++){
         cin >> arr[i];
     }
 
     for(int j = 1; j <= n; j++){
         for(int i = 0; i < n; i++){
             if(arr[i] == j){
-                cout << i+1 << " " << endl;
+                cout << i+1 << " ";
             }
         }
     }
