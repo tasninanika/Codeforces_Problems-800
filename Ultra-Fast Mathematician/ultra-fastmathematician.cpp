ca9713524c60c7084b5b834d310aa8f604240504
anikatasnin <jarintasnin09@gmail.com>
2025-01-17 20:07:08 +0600

    Added Ultra-Fast Mathematician

--- Ultra-Fast Mathematician/ultra-fastmathematician.cpp
+++ Ultra-Fast Mathematician/ultra-fastmathematician.cpp
@@ -5,10 +5,15 @@ int main(){
     int num1, num2;
     cin >> num1 >> num2;
 
-    for(int i = 0; i < num1.size(); i++){
-        for(int j = 0; j < num2.size(); j++){
-
-        }
+    string number1 = to_string(num1);
+    string number2 = to_string(num2);
+    for(int i = 0; i < number1.size(); i++){
+            if(number1[i] == number2[i]){
+                cout << 0;
+            }
+            else{
+                cout << 1;
+            }
     }
 
 
