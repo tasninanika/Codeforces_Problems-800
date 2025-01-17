1696b988c9f3f3d04115abcb7507df33436c7451
anikatasnin <jarintasnin09@gmail.com>
2025-01-17 21:27:34 +0600

    Added Is your horseshoe on the other hoof

--- Is your horseshoe on the other hoof/isyourhorseshoeontheotherhoof.cpp
+++ Is your horseshoe on the other hoof/isyourhorseshoeontheotherhoof.cpp
@@ -2,8 +2,18 @@
 using namespace std;
 
 int main(){
-    int s1, s2, s3, s4;
+    long long s1, s2, s3, s4;
     cin >> s1 >> s2 >> s3 >> s4;
+
+    int c = 0;
+
+    for(int i = 0; i < 4; i++){
+        for(j = 0; j < 4; j++){
+
+        }
+    }
+
+    cout << c << endl;
 
     return 0;
 }
