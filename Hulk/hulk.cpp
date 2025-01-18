ffb470cf265ef34e11ae2c4ac6130d0324f74253
anikatasnin <jarintasnin09@gmail.com>
2025-01-18 11:04:49 +0600

    Added Hulk

--- Hulk/hulk.cpp
+++ Hulk/hulk.cpp
@@ -2,12 +2,25 @@
 using namespace std;
 
 int main(){
-    int n;
+    int n, c = 0;
     cin >> n;
 
     for(int i = 0; i < n; i++){
-
+        if(n == i){
+            cout << "I hate";
+            c++;
+        }
+        else if(c == i){
+            cout << "I hate that I love it";
+            c++;
+        }
+        else{
+            cout << "I hate that I love it I hate it";
+            c++;
+        }
     }
+
+    cout << " it" << endl;
 
     return 0;
 }
