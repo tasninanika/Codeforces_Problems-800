16e416790a0228a6a92880e17db7991858592600
anikatasnin <jarintasnin09@gmail.com>
2025-01-20 15:12:10 +0600

    Added Pangram

--- Pangram/pangram.cpp
+++ Pangram/pangram.cpp
@@ -2,11 +2,17 @@
 using namespace std;
 
 int main(){
-    int n;
-    cin >> n;
+    int n, c = 0;
+    cin >> n >> c;
 
     string s;
     cin >> s;
+
+    for(int i = 0; i < s.size(); i++){
+        if(s[i] == s[i+1]){
+            c++;
+        }
+    }
 
     return 0;
 }
