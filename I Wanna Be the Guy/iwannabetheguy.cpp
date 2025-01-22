2211800db0a8066e3439f288587ea029d175ff79
anikatasnin <jarintasnin09@gmail.com>
2025-01-22 10:52:27 +0600

    Added I Wanna Be the Guy

--- I Wanna Be the Guy/iwannabetheguy.cpp
+++ I Wanna Be the Guy/iwannabetheguy.cpp
@@ -5,22 +5,27 @@ int main(){
     int n;
     cin >> n;
 
-    int p[n], q[n];
-    for(int i = 0; i < n; i++){
+    int p, q;
+    cin >> p >> q;
+    for(int i = 0; i < ; i++){
         cin >> p[i] >> q[i];
     }
 
     for(int i = 0; i < n; i++){
-        p[n].push_back(q[i]);
+        p[n + i] = q[i];
     }
 
     for(int i = 0; i < n; i++){
-        for(int j = 0; j < n - i - 1; j++){}
+        for(int j = 0; j < n - i - 1; j++){
             if(p[j] > p[j+1]){
                 swap(p[j], p[j+1]);
             }
+        }
     }
 
+    for (int i = 0; i < 2 * n; i++) {
+        cout << p[i] << " ";
+    }
 
 
     return 0;
