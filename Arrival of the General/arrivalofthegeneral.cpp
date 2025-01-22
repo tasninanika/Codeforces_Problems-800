f48221c0eb10cc52f4ceb2fcc03d19cd5655e683
anikatasnin <jarintasnin09@gmail.com>
2025-01-22 12:07:21 +0600

    Added  Arrival of the General

--- Arrival of the General/arrivalofthegeneral.cpp
+++ Arrival of the General/arrivalofthegeneral.cpp
@@ -2,7 +2,34 @@
 using namespace std;
 
 int main(){
+    int n, pos1 = 0, pos2 = 0, min_sec;
+    cin >> n;
 
+    int arr[n];
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+
+    int max_num = arr[0];
+    for(int i = 0; i < n; i++){
+        if(max_num < arr[i]){
+            max_num = arr[i];
+            swap(arr[0],arr[i]);
+            pos1 = i + 1;
+        }
+    }
+
+    int min_num = arr[0];
+    for(int i = 0; i < n; i++){
+        if(min_num >= arr[i]){
+            min_num = arr[i];
+            pos2 = i + 1;
+        }
+    }
+
+    min_sec = (pos1 - 1) + (n - pos2);
+
+    cout << min_sec << endl;
 
     return 0;
 }
