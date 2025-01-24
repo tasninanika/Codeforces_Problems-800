5fa88e294d863d0b036e65cb5c5714efe8f6abb3
anikatasnin <jarintasnin09@gmail.com>
2025-01-24 10:19:20 +0600

    Added Hit the Lottery

--- Hit the Lottery/hitthelottery.cpp
+++ Hit the Lottery/hitthelottery.cpp
@@ -5,32 +5,30 @@ int main(){
     int n, c = 0;
     cin >> n;
 
-    if(n >= 100){
-        n = n - 100;
-        c++;
-    }
-    else if(n >= 20 && n < 100){
-        n = n - 20;
-        c++;
-    }
-    else if(n >= 5 && n < 20){
-        n = n - 5;
-        c++;
-    }
-    else if(n >= 1 && n < 5){
-        n = n - 1;
-        c++;
-    }
-    else{
-        n = 0;
+    while(n != 0){
+        if(n >= 100){
+            n = n - 100;
+            c++;
+        }
+        else if(n >= 20 && n < 100){
+            n = n - 20;
+            c++;
+        }
+        else if(n >= 5 && n < 20){
+            n = n - 5;
+            c++;
+        }
+        else if(n >= 1 && n < 5){
+            n = n - 1;
+            c++;
+        }
+
     }
 
-    if(n == 0){
-        cout << 0 << endl;
-    }
-    else{
+
+
         cout << c << endl;
-    }
+
 
 
 
