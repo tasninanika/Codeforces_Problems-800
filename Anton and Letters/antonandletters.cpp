884a7adb05628a814c3218ae6fcb0a2d95b94587
anikatasnin <jarintasnin09@gmail.com>
2025-01-24 10:56:47 +0600

    Added Anton and Letters

--- Anton and Letters/antonandletters.cpp
+++ Anton and Letters/antonandletters.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 int main(){
+    int c = 0;
     string s;
     getline(cin,s);
 
@@ -12,6 +13,14 @@ int main(){
             }
         }
     }
+
+    for(int i = 0; i < s.size(); i++){
+        if(s[i] != s[i+1] && s[i] != '{' && s[i] != '}' && s[i] != ','){
+            c++;
+        }
+    }
+
+
     cout << s << endl;
 
     return 0;
