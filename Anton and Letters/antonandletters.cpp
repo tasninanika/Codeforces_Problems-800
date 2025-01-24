f4d4a24e3c5d408d083bee0bdb93d1b9eb98b6e8
anikatasnin <jarintasnin09@gmail.com>
2025-01-24 10:39:18 +0600

    Added Anton and Letters

--- Anton and Letters/antonandletters.cpp
+++ Anton and Letters/antonandletters.cpp
@@ -5,5 +5,13 @@ int main(){
     string s;
     cin >> s;
 
+    for(int i = 0; i < s.size(); i++){
+        for(int j = 0; j < s.size() - i - 1; j++){
+            if(s[j] > s[j+1]){
+                swap(s[j],s[j+1]);
+            }
+        }
+    }
+
     return 0;
 }
