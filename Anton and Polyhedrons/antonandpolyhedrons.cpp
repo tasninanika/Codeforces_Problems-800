8ff7257b7188cac64df1dc1dfef3994ac4805b05
anikatasnin <jarintasnin09@gmail.com>
2025-01-25 09:31:14 +0600

    Added Anton and Polyhedrons

--- Anton and Polyhedrons/antonandpolyhedrons.cpp
+++ Anton and Polyhedrons/antonandpolyhedrons.cpp
@@ -5,10 +5,32 @@ int main(){
     long long n;
     cin >> n;
 
+    int sum = 0;
+
     string s[n];
     for(int i = 0; i < n; i++){
         cin >> s[i];
     }
+
+    for(int i = 0; i < n; i++){
+        if(s[i] == "Tetrahedron"){
+            sum += 4;
+        }
+        else if(s[i] == "Cube"){
+            sum += 6;
+        }
+        else if(s[i] == "Octahedron"){
+            sum += 8;
+        }
+        else if(s[i] == "Dodecahedron"){
+            sum += 12;
+        }
+        else if(s[i] == "Icosahedron"){
+            sum += 20;
+        }
+    }
+
+    cout << sum << endl;
 
 
     return 0;
