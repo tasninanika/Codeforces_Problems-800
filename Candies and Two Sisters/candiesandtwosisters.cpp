189b96b7cf5415ce5980c195997df8d68082dcc2
anikatasnin <jarintasnin09@gmail.com>
2025-01-29 23:45:08 +0600

    Added Candies and Two Sisters

--- Candies and Two Sisters/candiesandtwosisters.cpp
+++ Candies and Two Sisters/candiesandtwosisters.cpp
@@ -1,26 +1,15 @@
 #include<iostream>
 using namespace std;
 
-int main(){
-    long long t;
+int main() {
+    int t;
     cin >> t;
 
-    long long n, a, b;
-    int c = 0;
-    for(int i = 0; i < t; i++){
+    for(int i = 0; i < t; i++) {
+        long long n;
         cin >> n;
-        for(int j = 1; j <= n; j++){
-            b = j;
-            a = n - b;
-            if(a > b){
-                c++;
-            }
-        }
+        cout << (n - 1) / 2 << endl;
     }
-
-    cout << c << endl;
-
-
 
     return 0;
 }
