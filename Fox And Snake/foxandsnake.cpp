4660eb49402532ad2d5d30ac07271da08a450e22
anikatasnin <jarintasnin09@gmail.com>
2025-01-31 11:36:25 +0600

    Added Fox And Snake

--- Fox And Snake/foxandsnake.cpp
+++ Fox And Snake/foxandsnake.cpp
@@ -8,7 +8,9 @@ int main(){
     for(int i = 1; i <= n; i++){
         for(int j = 0; j < m; j++){
             if(i % 2 == 0){
-                cout << ".";
+                if(j == m - 1){
+
+                }
             }
             else{
                 cout << "#";
