5332cf2a16364b597f57497989431279bcc48a82
anikatasnin <jarintasnin09@gmail.com>
2025-02-02 09:28:16 +0600

    Added Amusing Joke

--- Amusing Joke/amusingjoke.cpp
+++ Amusing Joke/amusingjoke.cpp
@@ -9,8 +9,16 @@ int main(){
 
     for(int i = 0; i < s3.size(); i++){
         for(int j = 0; j < s3.size() - i - 1; j++){
-            if(s[j] > s[j+1]){
-                swap(s[j],s[j+1]);
+            if(s3[j] > s3[j+1]){
+                swap(s3[j],s3[j+1]);
+            }
+        }
+    }
+
+    for(int i = 0; i < s4.size(); i++){
+        for(int j = 0; j < s4.size() - i - 1; j++){
+            if(s4[j] > s4[j+1]){
+                swap(s4[j],s4[j+1]);
             }
         }
     }
