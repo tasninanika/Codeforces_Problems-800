0968429c48de75409405cef6ff9214d5c7b006a9
anikatasnin <jarintasnin09@gmail.com>
2025-02-04 09:46:03 +0600

    Added  The New Year Meeting Friends

--- The New Year Meeting Friends/thenewyearmeetingfriends.cpp
+++ The New Year Meeting Friends/thenewyearmeetingfriends.cpp
@@ -2,8 +2,17 @@
 using namespace std;
 
 int main(){
-    int x1, x2, x3;
+    int x1, x2, x3, total;
     cin >> x1 >> x2 >> x3;
+
+    total = x1 + x2 + x3;
+
+    if(total % 2 == 0){
+        cout << total / 3 << endl;
+    }
+    else{
+        cout << (total  / 3) + 1 << endl;
+    }
 
     return 0;
 }
