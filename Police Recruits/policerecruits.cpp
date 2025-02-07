d1ebadfa28596c00665f897ae4b9d7ae0f9ef7c6
anikatasnin <jarintasnin09@gmail.com>
2025-02-07 23:12:16 +0600

    added Police Recruits

---
+++ Police Recruits/policerecruits.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n, p = 0, c = 0;
+    cin >> n;
+
+    for(int i = 0; i < n; i++){
+        int x;
+        cin >> x;
+    }
+
+    return 0;
+}
