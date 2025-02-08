aed68beff5d53eaa30a57e9be2ed849378cc467c
anikatasnin <jarintasnin09@gmail.com>
2025-02-08 09:20:43 +0600

    added Sum

--- Sum/sum.cpp
+++ Sum/sum.cpp
@@ -9,6 +9,10 @@ int main(){
     for(int i = 0; i < t; i++){
         int a , b, c;
         cin >> a >> b >> c;
+
+        if(a + b == c || a + c == b || b + c == a){
+            cout << "YES" << endl;
+        }
     }
 
     return 0;
