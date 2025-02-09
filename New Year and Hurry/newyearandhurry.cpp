cfa2cd6242c8d4372170fa3d92d1d86a10a68ea8
anikatasnin <jarintasnin09@gmail.com>
2025-02-09 09:23:49 +0600

    added New Year and Hurry

--- New Year and Hurry/newyearandhurry.cpp
+++ New Year and Hurry/newyearandhurry.cpp
@@ -2,13 +2,17 @@
 using namespace std;
 
 int main(){
-    int n, k;
+    int n, k, c = 0;
     cin >> n >> k;
 
     int total_time = 240 - k;
 
     for(int i = 0; i < n; i++){
        int each_problem_time = i * 5;
+
+       if(each_problem_time <= total_time){
+            c++;
+       }
     }
 
     return 0;
