694e74db6cd53824c387530a81db02c19315d094
anikatasnin <jarintasnin09@gmail.com>
2025-02-12 08:43:36 +0600

    added Soft Drinking

--- Soft Drinking/softdrinking.cpp
+++ Soft Drinking/softdrinking.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main(){
-    int n, k, l, c,, d, p, nl, np;
+    int n, k, l, c, d, p, nl, np;
     cin >> n >> k >> l >> c >> d >> p >> nl >> np;
 
     int total_drinks = k * l;
