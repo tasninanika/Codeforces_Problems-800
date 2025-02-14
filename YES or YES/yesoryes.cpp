5287e8b1ee84b2e65cb371b4db3f52e8102077ac
anikatasnin <jarintasnin09@gmail.com>
2025-02-14 15:59:21 +0600

    added YES or YES

--- YES or YES/yesoryes.cpp
+++ YES or YES/yesoryes.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
 int main(){
@@ -9,9 +9,9 @@ int main(){
     for(int i = 0; i < t; i++){
         cin >> s;
 
-        string str = tolower(s);
+        s[i] = tolower(s[i]);
 
-        if(s == 'yes'){
+        if(s == "yes"){
             cout << "YES" << endl;
         }
         else{
