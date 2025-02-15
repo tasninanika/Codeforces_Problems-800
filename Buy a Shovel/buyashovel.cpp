e2c303ae1516660ecfa4be3307a83e67f3e056ee
anikatasnin <jarintasnin09@gmail.com>
2025-02-15 11:28:21 +0600

    added Buy a Shovel

--- Buy a Shovel/buyashovel.cpp
+++ Buy a Shovel/buyashovel.cpp
@@ -7,6 +7,10 @@ int main(){
 
     for(int i = 1;;i++){
         sum += k;
+        if((sum % 10 == 0) || (sum % 10 == r)){
+            cout << i;
+            break;
+        }
     }
 
     return 0;
