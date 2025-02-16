20f0f08b57dda154c2ab904ebab9e40f05b989e3
anikatasnin <jarintasnin09@gmail.com>
2025-02-16 09:18:29 +0600

    added Restoring Three Numbers

--- Restoring Three Numbers/restoringthreenumbers.cpp
+++ Restoring Three Numbers/restoringthreenumbers.cpp
@@ -8,5 +8,13 @@ int main(){
         cin >> x[i];
     }
 
+    for(int i = 0; i < 4; i++){
+        for(int j = 0; j < 4 - i - 1; j++){
+            if(x[j] > x[j+1]){
+                swap(x[j],x[j+1]);
+            }
+        }
+    }
+
     return 0;
 }
