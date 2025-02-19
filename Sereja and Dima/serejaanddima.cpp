6346c4dce04ea34669dc6855bfcf2fe0cd4c1822
anikatasnin <jarintasnin09@gmail.com>
2025-02-19 22:56:21 +0600

    added Sereja and Dima

--- Sereja and Dima/serejaanddima.cpp
+++ Sereja and Dima/serejaanddima.cpp
@@ -4,12 +4,16 @@ using namespace std;
 
 int main(){
 
-    int n;
+    int n, sereja, dima;
     cin >> n;
 
     int arr[n];
     for(int i = 0; i < n ; i++){
         cin >> arr[i];
+    }
+
+    for(int i = 0; i < n/2; i++){
+        sereja =
     }
 
     return 0;
