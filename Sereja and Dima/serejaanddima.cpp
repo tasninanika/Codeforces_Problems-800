8de4b85a76d3c27df5fb3d29b1f92c0b5a646e5e
anikatasnin <jarintasnin09@gmail.com>
2025-02-19 23:39:04 +0600

    added Sereja and Dima

--- Sereja and Dima/serejaanddima.cpp
+++ Sereja and Dima/serejaanddima.cpp
@@ -1,68 +1,38 @@
-#include<bits/stdc++.h>
-using  namespace  std;
-int main()
-{
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
     int n;
     cin >> n;
 
-    int arr[n+3];
+    vector<int> arr(n);
 
-    for(int i = 0; i < n; i++){
+    for (int i = 0; i < n; i++) {
         cin >> arr[i];
     }
 
     int sereja = 0, dima = 0;
-    int l = 0;
-    int r = n-1;
+    int l = 0, r = n - 1;
     int z = 0;
 
-    while(l<=r)
-    {
-        if(z%2 == 0)
-        {
-            if(arr[l] > arr[r])
-            {
-                sereja += arr[l];
-                l++;
+    while (l <= r) {
+        if (z % 2 == 0) {
+            if (arr[l] > arr[r]) {
+                sereja += arr[l++];
+            } else {
+                sereja += arr[r--];
             }
-            else{
-                sereja += arr[r];
-                r--;
+        } else {
+            if (arr[l] > arr[r]) {
+                dima += arr[l++];
+            } else {
+                dima += arr[r--];
             }
-
-        }
-        else{
-            if(arr[l] > arr[r])
-            {
-                dima += arr[l];
-                l++;
-            }
-            else{
-                dima += arr[r];
-                r--;
-            }
-
         }
         z++;
     }
-    cout << sereja << " " << dima <<endl;
+
+    cout << sereja << " " << dima << endl;
 
     return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- 
