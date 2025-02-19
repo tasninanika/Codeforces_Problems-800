ced64f800aee81cbb871776e483820a3974b6da7
anikatasnin <jarintasnin09@gmail.com>
2025-02-19 23:32:23 +0600

    added Sereja and Dima

--- Sereja and Dima/serejaanddima.cpp
+++ Sereja and Dima/serejaanddima.cpp
@@ -1,25 +1,68 @@
-#include<iostream>
-
-using namespace std;
-
-int main(){
-
-    int n, sereja = 0, dima = 0;
+#include<bits/stdc++.h>
+using  namespace  std;
+int main()
+{
+    int n;
     cin >> n;
 
-    int arr[n];
-    for(int i = 0; i < n ; i++){
+    int arr[n+3];
+
+    for(int i = 0; i < n; i++){
         cin >> arr[i];
     }
 
-    if(n % 2 == 0){
-      for(int i = 0; i < n/2; i++){
-        sereja += arr[i];
-      }
+    int sereja = 0, dima = 0;
+    int l = 0;
+    int r = n-1;
+    int z = 0;
+
+    while(l<=r)
+    {
+        if(z%2 == 0)
+        {
+            if(arr[l] > arr[r])
+            {
+                sereja += arr[l];
+                l++;
+            }
+            else{
+                sereja += arr[r];
+                r--;
+            }
+
+        }
+        else{
+            if(arr[l] > arr[r])
+            {
+                dima += arr[l];
+                l++;
+            }
+            else{
+                dima += arr[r];
+                r--;
+            }
+
+        }
+        z++;
     }
-
-    cout << sereja << " ";
-
+    cout << sereja << " " << dima <<endl;
 
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
