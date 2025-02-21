e721f02f3800986dd7c04523845fbf0ee292a7c2
anikatasnin <jarintasnin09@gmail.com>
2025-02-21 09:30:17 +0600

    added Marathon

--- Marathon/marathon.cpp
+++ Marathon/marathon.cpp
@@ -8,6 +8,16 @@ int main(){
 
     for(int i = 0 ; i < t; i++){
         cin >> a >> b >> c >> d;
+
+        if(a > b){
+            c++;
+        }
+        else if(a > c){
+            c++;
+        }
+        else if(a > d){
+            c++;
+        }
     }
 
     return 0;
