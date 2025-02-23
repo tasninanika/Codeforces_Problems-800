4e15929f1c7260e1e0d97b034a4e65107e68f8fd
anikatasnin <jarintasnin09@gmail.com>
2025-02-23 09:25:13 +0600

    added Lucky

--- Lucky/lucky.cpp
+++ Lucky/lucky.cpp
@@ -14,6 +14,9 @@ int main(){
             sum1 = number[0] + number[1] + number[2];
             sum2 = number[3] + number[4] + number[5];
         }
+        if(sum1 == sum2){
+
+        }
     }
 
     return 0;
