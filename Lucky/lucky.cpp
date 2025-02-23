58e9bd73a3726825808e8d405aa65bfc3b80d11e
anikatasnin <jarintasnin09@gmail.com>
2025-02-23 09:33:32 +0600

    added Lucky

--- Lucky/lucky.cpp
+++ Lucky/lucky.cpp
@@ -8,10 +8,12 @@ int main(){
     int number;
     while(t--){
         cin >> number;
-
+        string str = to_string(number);
         int sum1, sum2;
-
-
+        for(int i = 0; i < str.size(); i++){
+            sum1 = str[0] + str[1] + str[2];
+            sum2 = str[3] + str[4] + str[5];
+        }
         if(sum1 == sum2){
             cout << "YES" << endl;
         }
