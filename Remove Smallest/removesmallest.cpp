1f8a7e1dfce07f63456e2c6187573411a40184e6
anikatasnin <jarintasnin09@gmail.com>
2025-02-25 23:05:00 +0600

    added Remove Smallest

--- Remove Smallest/removesmallest.cpp
+++ Remove Smallest/removesmallest.cpp
@@ -2,29 +2,5 @@
 #include <algorithm>
 using namespace std;
 
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int n;
-        cin >> n;
-        int a[50];
 
-        for (int i = 0; i < n; i++) {
-            cin >> a[i];
-        }
-
-        sort(a, a + n);
-
-        for (int i = 1; i < n; i++) {
-            if (a[i] - a[i - 1] > 1) {
-                cout << "NO" << endl;
-                goto next_case;
-            }
-        }
-        cout << "YES" << endl;
-        next_case:;
-    }
-    return 0;
-}
 
