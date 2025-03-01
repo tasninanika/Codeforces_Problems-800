bb95828f45c2144fabcf76a09aa161d44e432c97
anikatasnin <jarintasnin09@gmail.com>
2025-03-01 10:33:31 +0600

    added Borze

--- Borze/borze.cpp
+++ Borze/borze.cpp
@@ -9,12 +9,14 @@ int main(){
         if(s[i] == '.'){
             ternary_num += '0';
         }
-        else if(s[i] == '-'){
+        else if(s[i] == '-' && i < s.size()){
             if(s[i+1] == '.'){
                 ternary_num += '1';
+                i++;
         }
             else if(s[i+1] == '-'){
                 ternary_num += '2';
+                i++
             }
         }
     }
