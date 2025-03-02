fb0d099781fa03e5ecf515f6ec5043cc35c0e4f3
anikatasnin <jarintasnin09@gmail.com>
2025-03-02 09:55:52 +0600

    added Spy Detected!

--- Spy Detected!/spydetected.cpp
+++ Spy Detected!/spydetected.cpp
@@ -13,6 +13,14 @@ int main(){
         for(int i = 0; i < n; i++){
             cin >> arr[i];
         }
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+
+            }
+        }
+
+
     }
 
     return 0;
