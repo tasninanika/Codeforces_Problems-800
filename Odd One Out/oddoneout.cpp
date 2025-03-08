9293697d01e8e0634505749adcc379899caea8c4
anikatasnin <jarintasnin09@gmail.com>
2025-03-08 09:34:40 +0600

    added Odd One Out

--- Odd One Out/oddoneout.cpp
+++ Odd One Out/oddoneout.cpp
@@ -2,11 +2,12 @@
 using namespace std;
 
 int main(){
-    int t;
+    int t, a, b, c;
     cin >> t;
 
     while(t--){
         cin >> a >> b >> c;
+
         if(a != b || a != c){
             cout << a << endl;
         }
