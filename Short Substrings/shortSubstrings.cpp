3a63fe8aa64b54cada4cf369a32b3e7fdafba295
anikatasnin <jarintasnin09@gmail.com>
2025-03-21 20:21:45 +0600

    added Short Substrings

--- Short Substrings/shortSubstrings.cpp
+++ Short Substrings/shortSubstrings.cpp
@@ -10,8 +10,9 @@ int main(){
         cin >> b;
 
         string a;
+        a += b[0];
 
-        for(int i = 0; i < b.size(); i+=2){
+        for(int i = 1; i < b.size(); i+=2){
             a += b[i];
         }
 
