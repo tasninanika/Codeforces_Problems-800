d822452f7011cc927e0d2fe1dc2e711514ea4fc9
anikatasnin <jarintasnin09@gmail.com>
2025-03-24 11:35:55 +0600

    added Short Sort

--- Short Sort/shortSort.cpp
+++ Short Sort/shortSort.cpp
@@ -9,24 +9,9 @@ int main(){
         string s;
         cin >> s;
 
-        int c = 0;
 
-        if(s == "abc"){
+        if (s == "abc" || s == "acb" || s == "bac" || s == "cab") {
             cout << "YES" << endl;
-        }
-        for(int i = 0; i < s.size(); i++){
-            for(j = 0; j < s.size() - i - 1; j++){
-                if(s[j] > s[j+1]){
-                    swap(s[j],s[j+1]);
-                    c++;
-                }
-            }
-        }
-        if(c == 1){
-            cout << "YES" << endl;
-        }
-        else{
-            cout << "NO" << endl;
         }
     }
 
