4a0a60340523ab4de1d7c89d63f859e05f424560
anikatasnin <jarintasnin09@gmail.com>
2025-03-29 17:03:46 +0600

    added Stair, Peak, or Neither

--- Stair, Peak, or Neither/stair,Peak.OrNeither.cpp
+++ Stair, Peak, or Neither/stair,Peak.OrNeither.cpp
@@ -9,7 +9,7 @@ int main(){
         int a, b, c;
         cin >> a >> b >> c;
 
-        if(a < b < c){
+        if(a < b && b < c){
             cout << "STAIR" << endl;
         }
         else if(a < b > c){
