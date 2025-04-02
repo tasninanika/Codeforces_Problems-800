0dc4b1ab796675dae0f46d423b00ea7a5ce0a236
anikatasnin <jarintasnin09@gmail.com>
2025-04-02 08:40:34 +0600

    added Panoramix's Prediction

--- Panoramix's Prediction/panoramix'sPrediction.cpp
+++ Panoramix's Prediction/panoramix'sPrediction.cpp
@@ -5,24 +5,22 @@ int main(){
     int n, m;
     cin >> n >> m;
 
-    for(int next = n + 1; next <= m; next++){
+    for (int next = n + 1; ; next++) {
         int d;
-        for(d = 2; d < next; d++){
-            if(next % d == 0){
+        for (d = 2; d * d <= next; d++) {
+            if (next % d == 0) {
                 break;
             }
         }
-        if(d == next){
-            if(next == m){
+        if (d * d > next) {
+            if (next == m) {
                 cout << "YES" << endl;
                 return 0;
             }
-            else{
-                cout << "NO" << endl;
-                return 0;
-            }
+            break;
         }
     }
 
+    cout << "NO" << endl;
     return 0;
 }
