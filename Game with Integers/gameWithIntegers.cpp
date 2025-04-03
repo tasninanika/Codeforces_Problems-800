40eef2f772e2a55dfb29e6e87b60596b405584b0
anikatasnin <jarintasnin09@gmail.com>
2025-04-04 00:24:36 +0600

    added Game with Integers

--- Game with Integers/gameWithIntegers.cpp
+++ Game with Integers/gameWithIntegers.cpp
@@ -8,11 +8,11 @@ int main(){
     while(t--){
         int n;
         cin >> n;
-        if ((n-1) % 3 == 0 || (n+1) % 3 == 1) {
-            cout << "First" << endl;
+         if (n % 3 == 0) {
+            cout << "Second" << endl;
         }
         else {
-            cout << "Second" << endl;
+            cout << "First" << endl;
         }
 
     }
