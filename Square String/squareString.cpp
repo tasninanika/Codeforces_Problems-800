31a7c4fd96fdee4cd42b86f47c1351442ca6d63a
anikatasnin <jarintasnin09@gmail.com>
2025-04-14 10:56:02 +0600

    added Square String

--- Square String/squareString.cpp
+++ Square String/squareString.cpp
@@ -9,11 +9,21 @@ int main(){
         string s;
         cin >> s;
 
-        int right = s.size();
+        int l = s.size();
+
+        if(l % 2 != 0){
+            cout << "NO" << endl;
+
+        }
 
         for(int left = 0; left < right/2; left++){
             int arr1.push_back(left[i]);
         }
+
+        for(int r = n/2; r < right/2; r--){
+            int arr2.push_back(r[i]);
+        }
+
     }
 
     return 0;
