667ff679fff57178991104d705c27bc8689adb45
anikatasnin <jarintasnin09@gmail.com>
2025-04-20 09:01:44 +0600

    added Blank Space

--- Blank Space/blankSpace.cpp
+++ Blank Space/blankSpace.cpp
@@ -18,6 +18,9 @@ int main(){
         for(int i = 0; i < n; i++){
             if(a[i-1] == 0 && a[i] == a[i-1]){
                 c++;
+                if(c > max_c){
+
+                }
             }
             else{
                 c = 0;
