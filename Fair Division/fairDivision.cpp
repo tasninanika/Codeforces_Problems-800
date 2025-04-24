67e24d53d45fd4a7abd4a6cab8a74106b8a9cede
anikatasnin <jarintasnin09@gmail.com>
2025-04-24 10:28:42 +0600

    added Fair Division

--- Fair Division/fairDivision.cpp
+++ Fair Division/fairDivision.cpp
@@ -9,7 +9,7 @@ int main(){
         int n;
         cin >> n;
 
-        int a[n], one = 0, two = 0, sum = 0;
+        int a[n], one = 0, two = 0, sum;
         for(int i = 0; i < n; i++){
             cin >> a[i];
             if(a[i] == 1){
@@ -19,6 +19,8 @@ int main(){
                 two++;
             }
         }
+
+        sum = one + (2 * two);
 
         if(n % 2 == 0 && sum % 2 == 0){
             cout << "YES" << endl;
