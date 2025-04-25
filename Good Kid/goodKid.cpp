44ea8ec5d35eadee5360d6148d5781ffbc27bc9f
anikatasnin <jarintasnin09@gmail.com>
2025-04-25 11:00:14 +0600

    added Good Kid

--- Good Kid/goodKid.cpp
+++ Good Kid/goodKid.cpp
@@ -14,10 +14,9 @@ int main(){
             cin >> a[i];
         }
 
-        int product = 1;
+
         for(int i = 0; i < n; i++){
-            product *= a[i];
-        }
+
 
         cout << product << endl;
 
