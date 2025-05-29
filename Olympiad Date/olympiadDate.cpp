68ddf53caa34430fb56127154d6583da42922698
anikatasnin <jarintasnin09@gmail.com>
2025-05-29 10:09:19 +0600

    added Olympiad Date

--- Olympiad Date/olympiadDate.cpp
+++ Olympiad Date/olympiadDate.cpp
@@ -35,3 +35,10 @@ int main() {
                     break;
                 }
             }
+
+            if (ok) {
+                res = i + 1;
+                break;
+            }
+        }
+
