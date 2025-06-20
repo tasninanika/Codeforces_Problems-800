aba357255b62a9fc3a3384b82077a53e837a800d
anikatasnin <jarintasnin09@gmail.com>
2025-06-20 10:42:52 +0600

    added The Third Side

--- The Third Side/theThirdSide.cpp
+++ The Third Side/theThirdSide.cpp
@@ -1,1 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+
+        priority_queue<int, vector<int>, greater<int>> minHeap;
+
+        for (int i = 0; i < n; i++) {
+            int num;
+            cin >> num;
+            minHeap.push(num);
+        }
+
+        while (minHeap.size() > 1) {
+            int first = minHeap.top(); minHeap.pop();
+            int second = minHeap.top(); minHeap.pop();
+
+            int combined = first + second - 1;
+            minHeap.push(combined);
+        }
+
+        cout << minHeap.top() << endl;
+    }
+
+    return 0;
+}
 
