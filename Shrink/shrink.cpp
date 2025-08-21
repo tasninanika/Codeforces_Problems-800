e75d11f008503013762be0c6d7a2ff41c65b11fc
anikatasnin <jarintasnin09@gmail.com>
2025-08-21 22:48:44 +0600

    added Shrink

--- Shrink/shrink.cpp
+++ Shrink/shrink.cpp
@@ -1,1 +1,25 @@
+#include<iostream>
+using namespace std;
 
+int main(){
+    int t;
+    cin >> t;
+
+    while(t--){
+        int n;
+        cin >> n;
+
+        cout << 1 << " ";
+
+        for(int i = 3; i <= n; i++){
+            cout << i << " ";
+        }
+        if (n >= 2){
+          cout << 2;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+ 
